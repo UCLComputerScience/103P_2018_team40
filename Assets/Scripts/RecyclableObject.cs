b3a2bb5fc0aab88d980ddab8f3078db8a6072fd9
Walter Wu <walterwuyan@gmail.com>
2018-02-17 14:26:44 +0000

    Add recycle objects

---
+++ Assets/Scripts/RecyclableObject.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IRecycleComponent
+{
+    void Restart();
+    void ShutDown();
+}
+
+
+public class RecyclableObject : MonoBehaviour
+{
+    private List<IRecycleComponent> _recycleComponents;
+
+    private void Awake()
+    {
+        var components = GetComponents<MonoBehaviour>();
+        foreach (var com in components)
+        {
+            if (com is IRecycleComponent)
+            {
+                _recycleComponents.Add(com as IRecycleComponent);
+            }
+        }
+    }
+
+    public void Restart()
+    {
+        gameObject.SetActive(true);
+        foreach (var com in _recycleComponents)
+        {
+            com.Restart();
+        }
+    }
+
+    public void ShutDown()
+    {
+        gameObject.SetActive(false);
+        foreach (var com in _recycleComponents)
+        {
+            com.ShutDown();
+        }
+    }
+}
