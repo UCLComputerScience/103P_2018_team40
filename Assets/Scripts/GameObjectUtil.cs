b9e8734109126d35a385461853b5130cc88d2616
Walter Wu <walterwuyan@gmail.com>
2018-02-20 09:06:40 +0000

    Created new homescreen

---
+++ Assets/Scripts/GameObjectUtil.cs
@@ -0,0 +1,50 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//
+//public class GameObjectUtil
+//{
+//    private static Dictionary<RecyclableObject, ObjectPool> _pools = new Dictionary<RecyclableObject, ObjectPool>();
+//
+//    public static void Instantiate(GameObject prefab, Vector3 pos)
+//    {
+//        var recyclable = prefab.GetComponent<RecyclableObject>();
+//        if (recyclable != null) // the object is recyclable
+//        {
+//            // recycle the object
+//        }
+//        else
+//        {
+//            GameObject.Instantiate(prefab).transform.position = pos; // potential issues
+//        }
+//    }
+//
+//    public static void Destory(GameObject gameObject)
+//    {
+//        var recyclable = gameObject.GetComponent<RecyclableObject>();
+//        if (recyclable != null)
+//        {
+//            recyclable.ShutDown();
+//        }
+//        else
+//        {
+//            GameObject.Destroy(gameObject);
+//        }
+//    }
+//    private static ObjectPool getObjectPool(RecyclableObject reference)
+//    {
+//        
+//        if (_pools.ContainsKey(reference))
+//        {
+//            return _pools[reference];
+//        }
+//        
+//        var poolContainer = new GameObject(reference.gameObject.name + "ObjectPool");
+//        var pool = poolContainer.AddComponent<ObjectPool>();
+//        pool.prefab = reference;
+//        _pools.Add(reference, pool);
+//        
+//
+//        
+//    }
+//}
---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,25 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using UnityEngine;
+//
+//public class ObjectPool : MonoBehaviour
+//// this pool handles all types of recyclables
+//{
+//    public GameObject[] GameObjects;
+//
+//    // Use this for initialization
+//    void Start()
+//    {
+//    }
+//
+//    // Update is called once per frame
+//    void Update()
+//    {
+//    }
+//
+//    public void Recycle(GameObject prefab, Vector3 pos)
+//    {
+//
+//    }
+//}
