319542309c87b41bcaa64d5643f950aa4144f3d4
Walter Wu <walterwuyan@gmail.com>
2018-03-10 19:19:44 +0000

    Leaderboard with proper error messages

--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -11,13 +11,14 @@ public class LeaderboardManager : MonoBehaviour
 {
     public GameObject EntryPrefab;
     public GameObject NameInputField;
+    public GameObject ErrorText;
+    public GameObject InvalidNameText;
     public float EntryStartingY;
     public float EntryYSpacing;
-    public const string PhpUrl = "https://fizzyogaming6.azurewebsites.net/";
+    public const string PhpUrl = "https://fizzyogaming6.azurewebsites.net";
 
     private GameManager _gm;
     private string _name = "Default";
-
 
 
     private void Awake()
@@ -34,29 +35,53 @@ public class LeaderboardManager : MonoBehaviour
     {
         _gm.HideForLeaderboard();
         gameObject.SetActive(true);
+        ErrorText.SetActive(false);
+        InvalidNameText.SetActive(false);
         StartCoroutine(DisplayEntries());
     }
 
-    public IEnumerator DisplayEntries()
+    private IEnumerator DisplayEntries()
     {
+        yield return new WaitForSeconds(0.1f); // give enough time for insertion to apply
+        
+        // destory all previous entries
+        foreach (Transform en in transform)
+        {
+            if (en.name.StartsWith("Entry(Clone)"))
+            {
+                Destroy(en.gameObject);
+            }
+        }
+        // query db through php website
         var web = new WWW(PhpUrl);
         yield return web;
-        string[] entries = web.text.Split('\\');
-        Debug.Log(web.text);
-        for (int i = 0; i < entries.Length - 1; i++)
+        if (web.text == "" || web.text.StartsWith("Connection failed"))
         {
-            string[] fields = entries[i].Split(';');
-
-            var en = Instantiate(EntryPrefab);
-            en.transform.SetParent(transform, false);
-            var pos = en.transform.localPosition;
-            pos.y = EntryStartingY - i * EntryYSpacing;
-            en.transform.localPosition = pos;
-            en.GetComponent<Entry>().SetEntry(fields[0], fields[1],
-                fields[2]);
-            if (i >= 5)
+            DisplayError("Connection Failed!");
+        }
+        else if (web.text.StartsWith("Query failed"))
+        {
+            DisplayError("Query Failed!");
+        }
+        else
+        {
+            string[] entries = web.text.Split('\\');
+            Debug.Log(web.text);
+            for (int i = 0; i < entries.Length - 1; i++)
             {
-                break;
+                string[] fields = entries[i].Split(';');
+
+                var en = Instantiate(EntryPrefab);
+                en.transform.SetParent(transform, false);
+                var pos = en.transform.localPosition;
+                pos.y = EntryStartingY - i * EntryYSpacing;
+                en.transform.localPosition = pos;
+                en.GetComponent<Entry>().SetEntry((i + 1).ToString(), fields[0],
+                    fields[1]);
+                if (i >= 5)
+                {
+                    break;
+                }
             }
         }
     }
@@ -65,19 +90,25 @@ public class LeaderboardManager : MonoBehaviour
     {
         _name = NameInputField.GetComponent<InputField>().text;
         _name = _name == "" ? "Default" : _name;
+        if (_name.Contains(";") || _name.Contains("\\"))
+        {
+            InvalidNameText.SetActive(true);
+            return;
+        }
         var form = new WWWForm();
-        form.AddField("rank",1);
-        form.AddField("score",(int)_gm.Score);
-        form.AddField("name",_name);
+        form.AddField("score", (int) _gm.Score);
+        form.AddField("name", _name);
+        var www = new WWW(PhpUrl, form);
         
-        var www = new WWW(PhpUrl,form);
-        StartCoroutine(CheckWWW(www));
-
+        StartCoroutine(DisplayEntries());
+        InvalidNameText.SetActive(false);
     }
 
-    private IEnumerator CheckWWW(WWW w)
+
+
+    private void DisplayError(string msg)
     {
-        yield return w;
-        Debug.Log(w.text);
+        ErrorText.SetActive(true);
+        ErrorText.GetComponent<Text>().text = msg;
     }
 }
