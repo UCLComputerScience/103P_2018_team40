b757f2b7bca66dbf1d6b67b8a1befcd928d68314
PoojaSridhar16 <32365057+PoojaSridhar16@users.noreply.github.com>
2018-03-12 11:28:52 +0000

    fizzyodevice script added to assets folder

---
+++ Assets/FizzyoDevice.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using Fizzyo;
+
+
+
+public class FizzyoDevice : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        
+    }
+
+    public bool isButtonPressed()
+    {
+        bool buttonPressed = FizzyoFramework.Instance.Device.ButtonDown();
+        return buttonPressed;
+    }
+
+    public bool isBlow()
+    {
+        float pressure = FizzyoFramework.Instance.Device.Pressure();
+        if (pressure == 0)
+        {
+            return false;
+        }
+        return true;
+    }
+}
+
+
+
